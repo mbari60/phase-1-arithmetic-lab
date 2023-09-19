913ef997c86f135ecc1a0e8d0e795965051f7411
mbari60 <kevinmbari600@gmail.com>
2023-09-19 11:43:16 +0300

    arithmetics lab assiggnment done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 =25;
+let num4 =7;
+let mod = num3 % num4;
+
+let max = Math.max(11,14,18,12,7,10,20,3,19,5);
